fb7d32eb214755f2be2d154f1d8ef067d9a31859
Truitte <142534291+Truitte@users.noreply.github.com>
2023-12-14 13:42:07 -0500

    Create dice.py
    ff

---
+++ modules/dice.py
@@ -0,0 +1,22 @@
+# Dice rolling Module by Truitte Moreland, v0.0
+import random
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        numRolled += 1
+    return sum
+
+def rollDice(numDice, sizeDice):
+    numRolled = 0
+    sum = 0
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f"Sum: {sum}\n")
+        numRolled += 1
+    return sum
