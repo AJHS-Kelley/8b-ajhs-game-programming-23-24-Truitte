9e47ea74a263cbbf6a0eb77805bbc8f87b7e8fe4
Truitte <142534291+Truitte@users.noreply.github.com>
2024-01-30 14:34:01 -0500

    e
    e

---
+++ gameProgramming/04b_hangman/08_ultimateIntroPygame/tru.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(skysurface,(0,0))
+    
+    pygame.display.update()
+    clock.tick(60)
