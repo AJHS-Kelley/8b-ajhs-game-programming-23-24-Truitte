b8c0f0c2916675d78332f8342dfa990e35b80d15
Truitte <142534291+Truitte@users.noreply.github.com>
2024-04-17 14:12:25 -0400

    s
    s

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -1,30 +1,59 @@
-# Final Project, Truitte Moreland, v0.0
-import pygame
-from pygame.locals import *
-
+import pygame, sys
+ 
 pygame.init()
+ 
+WIDTH, HEIGHT = 900, 900
+ 
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic Tac Toe!")
 
-screen_width = 1000
-screen_height = 1000
+BOARD = pygame.image.load("assets/Board.png")
+X_IMG = pygame.image.load("assets/X.png")
+O_IMG = pygame.image.load("assets/O.png")
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Platformer')
+BG_COLOR = (214, 201, 227)
 
+board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+graphical_board = [[[None, None], [None, None], [None, None]], 
+                    [[None, None], [None, None], [None, None]], 
+                    [[None, None], [None, None], [None, None]]]
 
-#load images
-sun_img = pygame.image.load('sun.png')
-bg_img = pygame.image.load('img/sky.webp')
+to_move = 'X'
 
-run = True
-while run:
+SCREEN.fill(BG_COLOR)
+SCREEN.blit(BOARD, (64, 64))
 
-    screen.blit(bg_img, (0,0))
-    screen.blit(sun_img, (100, 100))
+pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def render_board(board, ximg, oimg):
+    global graphical_board
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 'X':
+                # Create an X image and rect
+                graphical_board[i][j][0] = ximg
+                graphical_board[i][j][1] = ximg.get_rect(center=(j*300+150, i*300+150))
+            elif board[i][j] == 'O':
+                graphical_board[i][j][0] = oimg
+                graphical_board[i][j][1] = oimg.get_rect(center=(j*300+150, i*300+150))
 
-    pygame.display.update()
+def add_XO(board, graphical_board, to_move):
+    current_pos = pygame.mouse.get_pos()
+    converted_x = (current_pos[0]-65)/835*2
+    converted_y = current_pos[1]/835*2
+    if board[round(converted_y)][round(converted_x)] != 'O' and board[round(converted_y)][round(converted_x)] != 'X':
+        board[round(converted_y)][round(converted_x)] = to_move
+        if to_move == 'O':
+            to_move = 'X'
+        else:
+            to_move = 'O'
+    
+    render_board(board, X_IMG, O_IMG)
 
-pygame.quit()
+    for i in range(3):
+        for j in range(3):
+            if graphical_board[i][j][0] is not None:
+                SCREEN.blit(graphical_board[i][j][0], graphical_board[i][j][1])
+    
+    return board, to_move
+
