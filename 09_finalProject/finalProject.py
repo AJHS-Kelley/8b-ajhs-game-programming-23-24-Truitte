ec5f5bb1fab1a7da97abb2143e231051fc4c39ea
Truitte <142534291+Truitte@users.noreply.github.com>
2024-04-19 14:36:10 -0400

    s
    s

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -89,3 +89,14 @@ def check_win(board):
         SCREEN.blit(graphical_board[2][2][0], graphical_board[2][2][1])
         pygame.display.update()
         return winner
+    
+    if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
+        winner =  board[0][2]
+        graphical_board[0][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[0][2][0], graphical_board[0][2][1])
+        graphical_board[1][1][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[1][1][0], graphical_board[1][1][1])
+        graphical_board[2][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[2][0][0], graphical_board[2][0][1])
+        pygame.display.update()
+        return winner
