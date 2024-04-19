3bd44a9225c5fa6bcfbbbdb73aae102fa3b08039
Truitte <142534291+Truitte@users.noreply.github.com>
2024-04-19 14:33:57 -0400

    s
    s

--- 09_finalProject/finalProject.py
+++ 09_finalProject/finalProject.py
@@ -57,6 +57,8 @@ def add_XO(board, graphical_board, to_move):
     
     return board, to_move
 
+game_finsihed = False
+
 def check_win(board):
     winner = None
     for row in range(0, 3):
@@ -68,13 +70,22 @@ def check_win(board):
             pygame.display.update()
             return winner
         
-        if (board[0][2] == board[1][1] == board[2][0]) and (board[0][2] is not None):
-        winner =  board[0][2]
-        graphical_board[0][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[0][2][0], graphical_board[0][2][1])
+    for col in range(0, 3):
+        if((board[0][col] == board[1][col] == board[2][col]) and (board[0][col] is not None)):
+            winner =  board[0][col]
+            for i in range(0, 3):
+                graphical_board[i][col][0] = pygame.image.load(f"assets/Winning {winner}.png")
+                SCREEN.blit(graphical_board[i][col][0], graphical_board[i][col][1])
+            pygame.display.update()
+            return winner
+   
+    if (board[0][0] == board[1][1] == board[2][2]) and (board[0][0] is not None):
+        winner =  board[0][0]
+        graphical_board[0][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[0][0][0], graphical_board[0][0][1])
         graphical_board[1][1][0] = pygame.image.load(f"assets/Winning {winner}.png")
         SCREEN.blit(graphical_board[1][1][0], graphical_board[1][1][1])
-        graphical_board[2][0][0] = pygame.image.load(f"assets/Winning {winner}.png")
-        SCREEN.blit(graphical_board[2][0][0], graphical_board[2][0][1])
+        graphical_board[2][2][0] = pygame.image.load(f"assets/Winning {winner}.png")
+        SCREEN.blit(graphical_board[2][2][0], graphical_board[2][2][1])
         pygame.display.update()
         return winner
