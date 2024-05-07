a4374308f18e8b3b2d57b5037da2b9b841caef0f
Truitte <142534291+Truitte@users.noreply.github.com>
2024-05-07 14:43:36 -0400

    s
    s

--- 09_finalProject/finalPorject2.py
+++ 09_finalProject/finalPorject2.py
@@ -4,7 +4,26 @@ import sys
 import random
 
 class snake(object):
-    pass
+    def _init_(self):
+        pass
+
+    def get_head_position(self):
+        pass
+    
+    def turn(self):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+    def handle_keys(self):
+        pass
 
 class food(object):
     pass
