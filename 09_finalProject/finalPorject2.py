f04361e6718959f5a6c46612da9ca23736ab485b
Truitte <142534291+Truitte@users.noreply.github.com>
2024-05-13 14:34:48 -0400

    s
    s

--- 09_finalProject/finalPorject2.py
+++ 09_finalProject/finalPorject2.py
@@ -34,4 +34,15 @@ class food(object):
 
     def draw(self, surface):
         pass
-    
+     
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 480
+
+GRIDSIZE = 20
+GRID_WIDTH = SCREEN_HEIGHT / GRIDSIZE
+GRID_HEIGHT = SCREEN_WIDTH / GRIDSIZE
+
+UP = (0,-1)
+DOWN = (0, 1)
+LEFT = (-1,0)
+RIGHT = (-1,0)
